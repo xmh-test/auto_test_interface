a63f04fadc7bb16693c322225781f49572c8e7b4
xmh-test <343200048@163.com>
2023-06-12 17:08:06 +0800

    增加响应数据xml格式转换为dict方法

--- apis/base_api.py
+++ apis/base_api.py
@@ -1,5 +1,8 @@
 import requests
+from requests import Response
+
 from utils.log_utils import logger
+import xmltodict
 
 
 class BaseApi:
@@ -10,7 +13,17 @@ class BaseApi:
         :param req: {mehtod: "GET", "url": "http://xxx.com", "params": {}, "json": {}}
         :return:
         """
+        # 增加请求超时时间
+        req["timeout"] = 5
         logger.info(f"发送请求的信息:{req}")
         r = requests.request(**req)
         logger.info(f"响应数据:{r.text}")
         return r
+
+    def response_to_dict(self, response: Response):
+        res_text = response.text
+        if res_text.startswith("<?xmh"):
+            final_dict = xmltodict.parse(res_text)
+        else:
+            final_dict = response.json()
+        return final_dict
